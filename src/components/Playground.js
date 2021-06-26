9991d8b3dadeb74dd175ec4147387427de7695f1
Z4CH1 <zacphd17@htl-kaindorf.ac.at>
2021-06-26 14:11:27 +0200

    Playground Frontend

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,7 +1,7 @@
 
 import {CustomWebsocket} from "../context/CustomWebsocket";
 import React, {Component}  from 'react';
-
+import './Playground.css';
 
 class Playground extends Component{
 
@@ -23,11 +23,36 @@ class Playground extends Component{
 
 
     render() {
+        const list = (this.state.lobbyObj.players).map((p) => <p key={p.userid}>{p.username}</p>);
+
         return (
-            <div>
-                <p>Playground</p>
-                <p>Rounds: {this.state.currentRound}/{this.state.lobbyObj.gameConfiguration.numberOfRounds}</p>
-                <p>Letter: {this.state.currentLetter}</p>
+            <div className="main-game">
+                <div className="container-game-header">
+                    <p id="title1">Stadt - Land - Fluss</p>
+                    <p id="title2">Game</p>
+                </div>
+                <div className="container-letter">
+                    <p id="letter">Letter: {this.state.currentLetter}</p><p id="numberofrounds">Round: {this.state.currentRound}/{this.state.lobbyObj.gameConfiguration.numberOfRounds}</p>
+                </div>
+                <div className="game">
+                    <p className="category">placeholder-Category1</p>
+                    <input className="cat-input" placeholder={this.state.currentLetter + "..."}/>
+                    <p className="category">placeholder-Category2</p>
+                    <input className="cat-input" placeholder={this.state.currentLetter + "..."}/>
+                    <p className="category">placeholder-Category3</p>
+                    <input className="cat-input" placeholder={this.state.currentLetter + "..."}/>
+                </div>
+                <div className="players">
+                    <p className="header-cat">Players</p>
+                    <div className="container-game-players">
+                        {list}
+                    </div>
+                </div>
+                <div className="container3" onClick={() => {
+                    //this.finishRound()
+                }}>
+                    <p className="btnText">Finish</p>
+                </div>
             </div>
         );
 
